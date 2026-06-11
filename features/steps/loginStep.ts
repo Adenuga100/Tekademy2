e0d0a3a8503e9a83cf9e19ea3cf9506d69d35d39
Adenuga100 <adenugaadeyemiisaac@gmail.com>
2026-06-11 12:06:58 +0100

    times

--- features/steps/loginStep.ts
+++ features/steps/loginStep.ts
@@ -69,6 +69,7 @@ When('User click on {string} button', async function (btn: string) {
 
 When('User click on {string} menu', async (menus: string) => {
     // You must pass the 'page' object to a static method
+    await expect(loginPage.menuButton(menus)).toBeVisible({ timeout: 60000 });
     await loginPage.clickMenu(menus); 
 });
 
