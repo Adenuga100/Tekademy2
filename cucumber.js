832331d54f054f7eb39330b8e75926fa04d5ecad
Adenuga100 <adenugaadeyemiisaac@gmail.com>
2026-07-07 20:23:31 +0100

    update

--- cucumber.js
+++ cucumber.js
@@ -1,3 +1,14 @@
+// {
+//   "default": {
+//     "requireModule": ['s-node/register'],
+//     "require": [
+//       'features/steps/**/*.ts',
+//       'hooks/**/*.ts'
+//     ],
+//     "format": ['progress']
+//   }
+// };
+
 module.exports = {
   default: {
     requireModule: ['ts-node/register'],
